291a5c44d317a163fdc9b2d9ac0f01b3eba38079
Malik K. <malik.krechiche@gmail.com>
2014-05-29 12:47:04 +0200

    Intégration HTML

---
+++ contact.php
@@ -0,0 +1,66 @@
+<!--
+   Accueil
+   Auteur : kipsafr.com
+   Crée en : 2014
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>CA - EXPANDMAKER</title>
+		<meta name="description" content="Vos idées : Nos projets">
+		<meta name="keywords" content="ExpandeMaker,ExpandMaking,CA,Services,Web,Graphisme,Site-Web,Serveur,Minecraft">
+		<meta name="author" content="kipsafr.com">
+		<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<!-- Contenu du site -->
+
+		<div id="background">
+
+			<div id="logo"><img src="images/logo.png"></div>
+
+			<!-- Menu -->
+
+			<div id="ACCUEIL"><a href="index.html"><h1>ACCUEIL</h1></a></div>
+
+			<div id="PROJETS"><a href="index.html"><h1>PROJETS</h1></a></div>
+
+			<div id="CONTACT"><a href="contact.php"><h1>CONTACT</h1></a></div>
+
+			<!-- Header -->
+
+			<div id="menu"></div>
+
+			<div id="slider_fond"><img src="images/slider_fond.png"></div>
+
+				<div id="slider"><img src="images/triangle.png"></div>
+	
+					<div id="bouton1"><a href="en_savoir_plus.php"><img src="images/bouton1.png"></div>
+
+					<div id="bouton1_text"><a href="en_savoir_plus.php"><img src="images/bouton1_text.png"></div>
+
+					<div id="bouton2_text"><a href="nos_services.php"><img src="images/bouton2_text.png"></a></div>
+
+					<div id="bouton2"><a href="nos_services.php"><img src="images/bouton2.png"></div>
+
+			
+			<div id="cache"><img src="images/cache.png"></div>
+		
+			<div id="slogan"><h1>VOS IDEES : NOS PROJETS</h1></div>
+
+			<div id="text_slider"><h1>« Internet sera à l’économie du 21è siècle ce que l’essence fut au 20è siècle. La puissance des ordinateurs c’est l’essence d’internet. »</h1></div>
+
+<div id="contact">
+
+<h1>Contact : </h1>
+
+<p>Veuillez nous envoyez un E-Mail à l'adresse suivante ou remplier ce formulaire : </p>
+
+</div>
+
+</body>
+
+
+</html>
---
+++ en_savoir_plus.php
@@ -0,0 +1,72 @@
+<!--
+   Accueil
+   Auteur : kipsafr.com
+   Crée en : 2014
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>CA - EXPANDMAKER</title>
+		<meta name="description" content="Vos idées : Nos projets">
+		<meta name="keywords" content="ExpandeMaker,ExpandMaking,CA,Services,Web,Graphisme,Site-Web,Serveur,Minecraft">
+		<meta name="author" content="kipsafr.com">
+		<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<!-- Contenu du site -->
+
+		<div id="background">
+
+			<div id="logo"><img src="images/logo.png"></div>
+
+			<!-- Menu -->
+
+			<div id="ACCUEIL"><a href="index.html"><h1>ACCUEIL</h1></a></div>
+
+			<div id="PROJETS"><a href="index.html"><h1>PROJETS</h1></a></div>
+
+			<div id="CONTACT"><a href="contact.php"><h1>CONTACT</h1></a></div>
+
+			<!-- Header -->
+
+			<div id="menu"></div>
+
+			<div id="slider_fond"><img src="images/slider_fond.png"></div>
+
+				<div id="slider"><img src="images/triangle.png"></div>
+
+					<div id="bouton1"><img src="images/bouton1.png"></div>
+
+					<div id="bouton1_text"><img src="images/bouton1_text.png"></div>
+
+					<div id="bouton2_text"><img src="images/bouton2_text.png"></a></div>
+			
+			<div id="cache"><img src="images/cache.png"></div>
+
+
+			<div id="bouton2"><img src="images/bouton2.png"></div>
+
+			
+			<div id="slogan"><h1>VOS IDEES : NOS PROJETS</h1></div>
+
+			<div id="text_slider"><h1>« Internet sera à l’économie du 21è siècle ce que l’essence fut au 20è siècle. La puissance des ordinateurs c’est l’essence d’internet. »</h1></div>
+
+<div id="contact">
+
+<h1>En savoir plus ?</h1>
+
+<p>Nous sommes une micro-entreprise francophone créee en 2014 dans le but d'aider les gens à réaliser leurs projets ou idées.
+Nous proposons nos services a différents tarifs, principalement des créations web ou graphiques ( tels que des sites-web ou logos...).</p>
+
+<h1>A propos du mètier d'ExpandMaking ? </h1>
+
+<p>ExpandMaker et un metier qui vient d'USA mais on voudrait le remettre en france Alors le principe est simple il faut prendre une idee des quelqu'un est l'étirée juqu'a ses limittes et donc de partir par exemple d'une video youtube A un jeu, un long-metrage, un livre, un site, une radio, ... etc. 5 % reviennent du revenue sur les differents projet revient au(x) créateur(s) de l'idée et le reste est redistribuer en fonction des travailleurs et du travaille fournit par chacun sur le projet.</p>
+
+</div>
+
+</body>
+
+
+</html>
